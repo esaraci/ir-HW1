aef35ed45a5d05f5a070c14140c688100b1fd42a
Eugen Saraci <eugen.saraci@gmail.com>
2018-12-10 12:25:19 +0100

    pushing hyp test

---
+++ hyp_testing.py
@@ -0,0 +1,4 @@
+# author    : Eugen Saraci
+# date      : 10/12/2018
+
+import scipy.stats as stats
--- plot_eval.py
+++ plot_eval.py
@@ -4,8 +4,11 @@
 
 import itertools
 import numpy as np
+import scipy.stats as stats
 import matplotlib.pyplot as plt
 from tabulate import tabulate
+from statsmodels.stats.multicomp import pairwise_tukeyhsd
+
 
 files = ["bm25_full.txt", "tf_idf_full.txt", "bm25_nostop.txt", "tf_idf_none.txt"]
 
@@ -53,8 +56,8 @@ for fname in files:
 plt.axvline(0.2, ls="--", c="black", lw=0.2)
 plt.axvline(0.8, ls="--", c="black", lw=0.2)
 plt.legend()
-plt.show()
-# plt.savefig("./figures/iprc.png")
+# plt.show()
+plt.savefig("./figures/iprc.png")
 plt.clf()
 
 
@@ -75,8 +78,8 @@ for fname in files:
     plt.plot(recalls, precisions, marker=next(markers), label=next(legend_label))
 
 plt.legend()
-# plt.savefig("./figures/prc.png")
-plt.show()
+#plt.show()
+plt.savefig("./figures/prc.png")
 plt.clf()
 
 
@@ -89,13 +92,66 @@ rprecs      = []
 precs_10    = []
 
 for fname in files:
-    evals       = np.loadtxt("./terrier/var/evaluation/{}".format(fname), skiprows=2, dtype=str)
+    evals = np.loadtxt("./terrier/var/evaluation/{}".format(fname), skiprows=2, dtype=str)
     aps.append([float(x[2]) for x in evals if x[1] == topic and x[0] == "map"][0])
     rprecs.append([float(x[2]) for x in evals if x[1] == topic and x[0] == "Rprec"][0])
     precs_10.append([float(x[2]) for x in evals if x[1] == topic and x[0] == "P_10"][0])
 
 
-print(tabulate(list(zip(legend_label, aps, rprecs, precs_10)), ["Modello", "MAP", "RPrec", "P@10"], tablefmt="latex"))
+# print(tabulate(list(zip(legend_label, aps, rprecs, precs_10)), ["Modello", "MAP", "RPrec", "P@10"], tablefmt="pipe"))
+
+
+
+################################
+# HYPOTHESIS TESTING
+################################
+
+aps         = []
+rprecs      = []
+precs_10    = []
+
+for fname in files:
+    evals = np.loadtxt("./terrier/var/evaluation/{}".format(fname), skiprows=2, dtype=str)
+    aps.append([float(x[2]) for x in evals if x[1] != topic and x[0] == "map"])
+    rprecs.append([float(x[2]) for x in evals if x[1] != topic and x[0] == "Rprec"])
+    precs_10.append([float(x[2]) for x in evals if x[1] != topic and x[0] == "P_10"])
+
+
+
+# needed for pairwise_tukeyhsd
+names = np.repeat("bm25_full", 50)
+names = np.append(names, np.repeat("tf_idf_full", 50))
+names = np.append(names, np.repeat("bm25_nostop", 50))
+names = np.append(names, np.repeat("tf_idf_none", 50))
+
+aps         = np.array(aps)
+rprecs      = np.array(rprecs)
+precs_10    = np.array(precs_10)
+
+
+plt.boxplot([aps[0], aps[1], aps[2], aps[3]], vert=False, labels=["bm25_full", "tf_idf_full", "bm25_nostop", "tf_idf_none"])
+plt.show()
+
+# needed for pairwise_tukeyhsd
+aps.shape       = (200,)
+rprecs.shape    = (200,)
+precs_10.shape  = (200,)
+
+tukey_map       = pairwise_tukeyhsd(aps, names)
+tukey_rprecs    = pairwise_tukeyhsd(rprecs, names)
+tukey_precs_10  = pairwise_tukeyhsd(precs_10, names)
+
+print(tukey_map)
+tukey_map.plot_simultaneous("bm_25_full").savefig("./figures/tukey_map.png")
+tukey_rprecs.plot_simultaneous("bm_25_full").savefig("./figures/tukey_rprecs.png")
+tukey_precs_10.plot_simultaneous("bm_25_full").savefig("./figures/tukey_precs_10.png")
+
+# print(stats.f_oneway(aps[0], aps[1], aps[2], aps[3]))
+# print(stats.f_oneway(rprecs[0], rprecs[1], rprecs[2], rprecs[3]))
+# print(stats.f_oneway(precs_10[0], precs_10[1], precs_10[2], precs_10[3]))
+
+
+
 """
 evals = np.loadtxt("./terrier/var/evaluation/{}".format(files[0]), skiprows=2, dtype=str)
 
@@ -107,4 +163,6 @@ print(aps)
 plt.figure()
 plt.bar(np.arange(351, 401), aps)
 plt.show()
+
 """
+
